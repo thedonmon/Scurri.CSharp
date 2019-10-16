e29ab10ef3274025cb07ce578a592c0a1b02be7b
dmaman <dmaman@shipbob.com>
2019-10-15 21:12:12 -0500

    add simpleinjector registration if needed

---
+++ Scurri.Client/Scurri.Tests.Core/SimpleInjectorContainerTest.cs
@@ -0,0 +1,37 @@
+﻿using Scurri.Client;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Scurri.Tests.Core
+{
+    public class SimpleInjectorContainerTest : IClassFixture<ScurriFixture>
+    {
+        private readonly ScurriFixture _fixture;
+        public SimpleInjectorContainerTest(ScurriFixture fixture)
+        {
+            _fixture = fixture;
+        }
+        [Fact]
+        public void CanVerifyContainer()
+        {
+            var container = new Container();
+            container.Options.AllowOverridingRegistrations = false;
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+            var config = new ScurriConfiguration
+            {
+                CompanySlug = _fixture.configuration["company-slug"],
+                AuthToken = _fixture.configuration["authtoken"],
+                UserName = _fixture.configuration["loginname"],
+                Secret = _fixture.configuration["secret"]
+            };
+            container.RegisterInstance<IScurriConfiguration>(config);
+            container.Register<IRestScurriApiClient,ScurriClient>(Lifestyle.Scoped);
+            container.Verify();
+
+        }
+    }
+}
